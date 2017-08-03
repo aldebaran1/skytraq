7a49e0ea4b67d8b14adc2ac89d2115e21102e1fb
C-Callejas <callejas@bu.edu>
2017-08-03 09:44:10 -0400

    Added duplicate file checking, while loop

--- filechecker.py
+++ filechecker.py
@@ -12,32 +12,39 @@ import time
 import socket
 
 pin = 11
+counter=0
 
 GPIO.cleanup
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.OUT)
 
-"""following block is a test block, to be commented out after testing GPIO pin
-GPIO.output(pin, GPIO.HIGH)
-time.sleep(5)
-GPIO.output(pin, GPIO.LOW)
-block end"""
-
-
-usr = socket.gethostname()        
-t = datetime.datetime.utcnow()
-#tm = t.strftime('%Y%m%d')
-#dd1 = t.strftime('%d')
-#mm1 = t.strftime('%m')
-yy1 = t.strftime('%y')
-day_in_year = t.timetuple().tm_yday
-path = os.path.dirname(os.path.realpath(__file__))
-fn = path+'/data/'+usr+'0'+str(day_in_year)+'.'+yy1+'r' 
-sizeA =int( os.path.getsize(fn) )
-time.sleep(60)
-sizeB =int( os.path.getsize(fn) )
-
-if sizeB > sizeA:
-  GPIO.output(pin, GPIO.HIGH)
-if sizeB <= sizeA:
-  GPIO.output(pin, GPIO.LOW)
+while True:
+    usr = socket.gethostname()        
+    t = datetime.datetime.utcnow()
+    tm = t.strftime('%Y%m%d')
+    dd1 = t.strftime('%d')
+    mm1 = t.strftime('%m')
+    yy1 = t.strftime('%y')
+    day_in_year = t.timetuple().tm_yday
+    path = os.path.dirname(os.path.realpath(__file__))
+    fn = path+'/data/'+usr+'0'+str(day_in_year)+'.'+yy1+'r' 
+    
+    sizeA =int( os.path.getsize(fn) )
+    time.sleep(30)
+    sizeB =int( os.path.getsize(fn) )
+    
+    if sizeB > sizeA:
+        counter=0
+        GPIO.output(pin, GPIO.HIGH)        
+    else:
+        counter += 1    
+        fn= fn+"_"+str(counter)
+        sizeA = int( os.path.getsize(fn) )
+        time.sleep(30)
+        sizeB = int( os.path.getsize(fn) )
+        
+        if sizeB > sizeA:
+            GPIO.output(pin, GPIO.HIGH)
+            counter -= 1
+        else:
+            GPIO.output(pin, GPIO.LOW)
