c2b38d6bd3e7f041dc32e1b7784a811199617be2
aldebaran1 <aldebaran1>
2017-08-09 18:36:47 +0000

    using glob and time.mktime to check the sizes

--- filechecker.py
+++ filechecker.py
@@ -10,13 +10,16 @@ import datetime
 import os.path
 import time
 import socket
+import glob
 
 pin = 11
-counter=0
 
-GPIO.cleanup
+GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.OUT)
+
+dt = datetime.datetime.now()
+t1 = time.mktime(dt.timetuple())
 
 while True:
     usr = socket.gethostname()        
@@ -26,25 +29,25 @@ while True:
     mm1 = t.strftime('%m')
     yy1 = t.strftime('%y')
     day_in_year = t.timetuple().tm_yday
-    path = os.path.dirname(os.path.realpath(__file__))
-    fn = path+'/data/'+usr+'0'+str(day_in_year)+'.'+yy1+'r' 
-    
-    sizeA =int( os.path.getsize(fn) )
-    time.sleep(30)
-    sizeB =int( os.path.getsize(fn) )
-    
-    if sizeB > sizeA:
-        counter=0
-        GPIO.output(pin, GPIO.HIGH)        
-    else:
-        counter += 1    
-        fn= fn+"_"+str(counter)
-        sizeA = int( os.path.getsize(fn) )
-        time.sleep(30)
-        sizeB = int( os.path.getsize(fn) )
+    path = os.path.dirname(os.path.realpath(__file__))     
+   
+    wlst = path+'/data/'+usr+'0'+str(day_in_year)+'*'
+    flist =sorted(glob.glob(wlst))
+       
+    try:
+	
+        sizeA = int( os.path.getsize(flist[-1]) )
+        dt = datetime.datetime.now()
+        t2 = time.mktime(dt.timetuple())
         
-        if sizeB > sizeA:
-            GPIO.output(pin, GPIO.HIGH)
-            counter -= 1
-        else:
-            GPIO.output(pin, GPIO.LOW)
+        if (t1 + 30) <= t2:
+            print (flist)
+            print (t1, t2, sizeA)
+            sizeB = int( os.path.getsize(flist[-1]) )
+            t1 = t2    
+            if sizeB > sizeA:
+                GPIO.output(pin, GPIO.HIGH)        
+            else:
+                GPIO.output(pin, GPIO.LOW)
+    except:
+        print('There is no files in the directory')
